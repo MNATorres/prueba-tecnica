170448f2969ef7c878cd1b347c8311dae7d629ee
Federico Jure <mtri21019@gmail.com>
2023-01-28 21:34:10 -0300

    component MainTable

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import React from "react";
 import Date from "./component/dates/Date";
 import { DataPicker } from "./component/dates/DatePicker";
+import MainTable from "./component/table/MainTable";
 
 function App() {
   return (
     <div>
       <Date />
+      <MainTable />
     </div>
   );
 }
