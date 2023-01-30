fa1d0d0002faede998e6a7291ed5434b9198678e
Federico Jure <mtri21019@gmail.com>
2023-01-30 08:50:23 -0300

    header table

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,4 @@
-import React from "react";
 import Date from "./component/dates/Date";
-import { DataPicker } from "./component/dates/DatePicker";
 import MainTable from "./component/table/MainTable";
 
 function App() {
--- src/component/table/MainTable.tsx
+++ src/component/table/MainTable.tsx
@@ -1,7 +1,6 @@
-import React, { HTMLAttributes, HTMLProps } from 'react'
-import ReactDOM from 'react-dom/client'
+import React, { HTMLAttributes, HTMLProps } from "react";
 
-import './MainTable.css';
+import "./MainTable.css";
 
 import {
   Column,
@@ -14,39 +13,21 @@ import {
   getExpandedRowModel,
   ColumnDef,
   flexRender,
-} from '@tanstack/react-table'
-import { makeData, Person } from './makeData';
+} from "@tanstack/react-table";
+import { makeData, Person } from "./makeData";
 
 export default function MainTable() {
-  const rerender = React.useReducer(() => ({}), {})[1]
+  const rerender = React.useReducer(() => ({}), {})[1];
 
   const columns = React.useMemo<ColumnDef<Person>[]>(
     () => [
       {
-        header: 'Name',
-        footer: props => props.column.id,
+        header: " ",
+        footer: (props) => props.column.id,
         columns: [
           {
-            accessorKey: 'firstName',
-            header: ({ table }) => (
-              <>
-                <IndeterminateCheckbox
-                  {...{
-                    checked: table.getIsAllRowsSelected(),
-                    indeterminate: table.getIsSomeRowsSelected(),
-                    onChange: table.getToggleAllRowsSelectedHandler(),
-                  }}
-                />{' '}
-                <button
-                  {...{
-                    onClick: table.getToggleAllRowsExpandedHandler(),
-                  }}
-                >
-                  {table.getIsAllRowsExpanded() ? '👇' : '👉'}
-                </button>{' '}
-                First Name
-              </>
-            ),
+            accessorKey: "firstName",
+            header: ({ table }) => <>Vendedor</>,
             cell: ({ row, getValue }) => (
               <div
                 style={{
@@ -55,82 +36,196 @@ export default function MainTable() {
                   // and paddingLeft to visually indicate the depth
                   // of the row
                   paddingLeft: `${row.depth * 2}rem`,
+                  
                 }}
               >
+                  
                 <>
-                  <IndeterminateCheckbox
-                    {...{
-                      checked: row.getIsSelected(),
-                      indeterminate: row.getIsSomeSelected(),
-                      onChange: row.getToggleSelectedHandler(),
-                    }}
-                  />{' '}
+                  {" "}
                   {row.getCanExpand() ? (
                     <button
                       {...{
                         onClick: row.getToggleExpandedHandler(),
-                        style: { cursor: 'pointer' },
+                        style: { cursor: "pointer" },
                       }}
                     >
-                      {row.getIsExpanded() ? '👇' : '👉'}
+                      {row.getIsExpanded() ? "👇" : "👉"}
                     </button>
                   ) : (
-                    '🔵'
-                  )}{' '}
+                    "🔵"
+                  )}{" "}
                   {getValue()}
                 </>
               </div>
             ),
-            footer: props => props.column.id,
+            footer: (props) => props.column.id,
           },
           {
-            accessorFn: row => row.lastName,
-            id: 'lastName',
-            cell: info => info.getValue(),
-            header: () => <span>Last Name</span>,
-            footer: props => props.column.id,
+            accessorFn: (row) => row.lastName,
+            id: "lastName",
+            cell: (info) => info.getValue(),
+            header: () => <span>Fecha Alta</span>,
+            footer: (props) => props.column.id,
+          },
+          {
+            accessorFn: (row) => row.lastName,
+            id: "lastName",
+            cell: (info) => info.getValue(),
+            header: () => <span>Fecha Baja</span>,
+            footer: (props) => props.column.id,
+          },
+          {
+            accessorFn: (row) => row.lastName,
+            id: "lastName",
+            cell: (info) => info.getValue(),
+            header: () => <span>Ingresadas</span>,
+            footer: (props) => props.column.id,
+          },
+          {
+            accessorFn: (row) => row.lastName,
+            id: "lastName",
+            cell: (info) => info.getValue(),
+            header: () => <span>Ventas MP</span>,
+            footer: (props) => props.column.id,
+          },
+          {
+            accessorFn: (row) => row.lastName,
+            id: "lastName",
+            cell: (info) => info.getValue(),
+            header: () => <span>Cruce Scoring</span>,
+            footer: (props) => props.column.id,
+          },
+          {
+            accessorFn: (row) => row.lastName,
+            id: "lastName",
+            cell: (info) => info.getValue(),
+            header: () => <span>Objetivo</span>,
+            footer: (props) => props.column.id,
+          },
+          {
+            accessorFn: (row) => row.lastName,
+            id: "lastName",
+            cell: (info) => info.getValue(),
+            header: () => <span>Producción</span>,
+            footer: (props) => props.column.id,
           },
         ],
       },
       {
-        header: 'Info',
-        footer: props => props.column.id,
+        header: "CLASIFICACIONES PENDIENTES",
+        footer: (props) => props.column.id,
         columns: [
-          {
-            accessorKey: 'age',
-            header: () => 'Age',
-            footer: props => props.column.id,
-          },
-          {
-            header: 'More Info',
-            columns: [
+          
+          
               {
-                accessorKey: 'visits',
-                header: () => <span>Visits</span>,
-                footer: props => props.column.id,
+                accessorKey: "visits",
+                header: () => <span>2</span>,
+                footer: (props) => props.column.id,
               },
               {
-                accessorKey: 'status',
-                header: 'Status',
-                footer: props => props.column.id,
+                accessorKey: "status",
+                header: "4",
+                footer: (props) => props.column.id,
               },
               {
-                accessorKey: 'progress',
-                header: 'Profile Progress',
-                footer: props => props.column.id,
+                accessorKey: "progress",
+                header: "5",
+                footer: (props) => props.column.id,
+              },
+              {
+                accessorKey: "progress",
+                header: "6",
+                footer: (props) => props.column.id,
+              },
+              {
+                accessorKey: "progress",
+                header: "7",
+                footer: (props) => props.column.id,
+              },
+              {
+                accessorKey: "progress",
+                header: "SUBTOTAL",
+                footer: (props) => props.column.id,
+              },
+              {
+                accessorKey: "progress",
+                header: "3",
+                footer: (props) => props.column.id,
+              },
+              {
+                accessorKey: "progress",
+                header: "8",
+                footer: (props) => props.column.id,
+              },
+              {
+                accessorKey: "progress",
+                header: "9",
+                footer: (props) => props.column.id,
+              },
+              {
+                accessorKey: "progress",
+                header: "SUBTOTAL",
+                footer: (props) => props.column.id,
+              },
+              {
+                accessorKey: "progress",
+                header: "Anulada 3 + 7",
+                footer: (props) => props.column.id,
+              },
+              {
+                accessorKey: "progress",
+                header: "Anulada Rechazada",
+                footer: (props) => props.column.id,
               },
             ],
-          },
-        ],
+         
       },
+      //////////////////////////
+      {
+        header: "MESES ANTERIORES",
+        footer: (props) => props.column.id,
+        columns: [
+          
+          
+              {
+                accessorKey: "visits",
+                header: () => <span>-1</span>,
+                footer: (props) => props.column.id,
+              },
+              {
+                accessorKey: "status",
+                header: "-2",
+                footer: (props) => props.column.id,
+              },
+              {
+                accessorKey: "progress",
+                header: "-3",
+                footer: (props) => props.column.id,
+              },
+              {
+                accessorKey: "progress",
+                header: "PROM",
+                footer: (props) => props.column.id,
+              },
+              {
+                accessorKey: "progress",
+                header: "GB",
+                footer: (props) => props.column.id,
+              },
+            ],
+         
+      },
+
+      
+      //////////////////////////
     ],
     []
-  )
+  );
 
-  const [data, setData] = React.useState(() => makeData(100, 5, 3))
-  const refreshData = () => setData(() => makeData(100, 5, 3))
+  const [data, setData] = React.useState(() => makeData(100, 5, 3));
+  const refreshData = () => setData(() => makeData(100, 5, 3));
 
-  const [expanded, setExpanded] = React.useState<ExpandedState>({})
+  const [expanded, setExpanded] = React.useState<ExpandedState>({});
 
   const table = useReactTable({
     data,
@@ -139,22 +234,22 @@ export default function MainTable() {
       expanded,
     },
     onExpandedChange: setExpanded,
-    getSubRows: row => row.subRows,
+    getSubRows: (row) => row.subRows,
     getCoreRowModel: getCoreRowModel(),
     getPaginationRowModel: getPaginationRowModel(),
     getFilteredRowModel: getFilteredRowModel(),
     getExpandedRowModel: getExpandedRowModel(),
     debugTable: true,
-  })
+  });
 
   return (
     <div className="p-2">
       <div className="h-2" />
       <table>
         <thead>
-          {table.getHeaderGroups().map(headerGroup => (
+          {table.getHeaderGroups().map((headerGroup) => (
             <tr key={headerGroup.id}>
-              {headerGroup.headers.map(header => {
+              {headerGroup.headers.map((header) => {
                 return (
                   <th key={header.id} colSpan={header.colSpan}>
                     {header.isPlaceholder ? null : (
@@ -163,24 +258,20 @@ export default function MainTable() {
                           header.column.columnDef.header,
                           header.getContext()
                         )}
-                        {header.column.getCanFilter() ? (
-                          <div>
-                            <Filter column={header.column} table={table} />
-                          </div>
-                        ) : null}
+                        {header.column.getCanFilter() ? <div></div> : null}
                       </div>
                     )}
                   </th>
-                )
+                );
               })}
             </tr>
           ))}
         </thead>
         <tbody>
-          {table.getRowModel().rows.map(row => {
+          {table.getRowModel().rows.map((row) => {
             return (
               <tr key={row.id}>
-                {row.getVisibleCells().map(cell => {
+                {row.getVisibleCells().map((cell) => {
                   return (
                     <td key={cell.id}>
                       {flexRender(
@@ -188,10 +279,10 @@ export default function MainTable() {
                         cell.getContext()
                       )}
                     </td>
-                  )
+                  );
                 })}
               </tr>
-            )
+            );
           })}
         </tbody>
       </table>
@@ -202,33 +293,33 @@ export default function MainTable() {
           onClick={() => table.setPageIndex(0)}
           disabled={!table.getCanPreviousPage()}
         >
-          {'<<'}
+          {"<<"}
         </button>
         <button
           className="border rounded p-1"
           onClick={() => table.previousPage()}
           disabled={!table.getCanPreviousPage()}
         >
-          {'<'}
+          {"<"}
         </button>
         <button
           className="border rounded p-1"
           onClick={() => table.nextPage()}
           disabled={!table.getCanNextPage()}
         >
-          {'>'}
+          {">"}
         </button>
         <button
           className="border rounded p-1"
           onClick={() => table.setPageIndex(table.getPageCount() - 1)}
           disabled={!table.getCanNextPage()}
         >
-          {'>>'}
+          {">>"}
         </button>
         <span className="flex items-center gap-1">
           <div>Page</div>
           <strong>
-            {table.getState().pagination.pageIndex + 1} of{' '}
+            {table.getState().pagination.pageIndex + 1} of{" "}
             {table.getPageCount()}
           </strong>
         </span>
@@ -237,20 +328,20 @@ export default function MainTable() {
           <input
             type="number"
             defaultValue={table.getState().pagination.pageIndex + 1}
-            onChange={e => {
-              const page = e.target.value ? Number(e.target.value) - 1 : 0
-              table.setPageIndex(page)
+            onChange={(e) => {
+              const page = e.target.value ? Number(e.target.value) - 1 : 0;
+              table.setPageIndex(page);
             }}
             className="border p-1 rounded w-16"
           />
         </span>
         <select
           value={table.getState().pagination.pageSize}
-          onChange={e => {
-            table.setPageSize(Number(e.target.value))
+          onChange={(e) => {
+            table.setPageSize(Number(e.target.value));
           }}
         >
-          {[10, 20, 30, 40, 50].map(pageSize => (
+          {[10, 20, 30, 40, 50].map((pageSize) => (
             <option key={pageSize} value={pageSize}>
               Show {pageSize}
             </option>
@@ -266,81 +357,5 @@ export default function MainTable() {
       </div>
       <pre>{JSON.stringify(expanded, null, 2)}</pre>
     </div>
-  )
+  );
 }
-
-function Filter({
-  column,
-  table,
-}: {
-  column: Column<any, any>
-  table: Table<any>
-}) {
-  const firstValue = table
-    .getPreFilteredRowModel()
-    .flatRows[0]?.getValue(column.id)
-
-  const columnFilterValue = column.getFilterValue()
-
-  return typeof firstValue === 'number' ? (
-    <div className="flex space-x-2">
-      <input
-        type="number"
-        value={(columnFilterValue as [number, number])?.[0] ?? ''}
-        onChange={e =>
-          column.setFilterValue((old: [number, number]) => [
-            e.target.value,
-            old?.[1],
-          ])
-        }
-        placeholder={`Min`}
-        className="w-24 border shadow rounded"
-      />
-      <input
-        type="number"
-        value={(columnFilterValue as [number, number])?.[1] ?? ''}
-        onChange={e =>
-          column.setFilterValue((old: [number, number]) => [
-            old?.[0],
-            e.target.value,
-          ])
-        }
-        placeholder={`Max`}
-        className="w-24 border shadow rounded"
-      />
-    </div>
-  ) : (
-    <input
-      type="text"
-      value={(columnFilterValue ?? '') as string}
-      onChange={e => column.setFilterValue(e.target.value)}
-      placeholder={`Search...`}
-      className="w-36 border shadow rounded"
-    />
-  )
-}
-
-function IndeterminateCheckbox({
-  indeterminate,
-  className = '',
-  ...rest
-}: { indeterminate?: boolean } & HTMLProps<HTMLInputElement>) {
-  const ref = React.useRef<HTMLInputElement>(null!)
-
-  React.useEffect(() => {
-    if (typeof indeterminate === 'boolean') {
-      ref.current.indeterminate = !rest.checked && indeterminate
-    }
-  }, [ref, indeterminate])
-
-  return (
-    <input
-      type="checkbox"
-      ref={ref}
-      className={className + ' cursor-pointer'}
-      {...rest}
-    />
-  )
-}
-
-
